ffd227d2e9092e79297cfd3f2398fefb687f3f2e
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-04-04 17:20:04 +0530

    weather controller.js completed

--- src/controllers/WeatherController.js
+++ src/controllers/WeatherController.js
@@ -37,3 +37,51 @@ router.patch('/weather/lk/byDistrict', async (req, res) => {
     }
 });
 
+router.get('/weather/lk/byDate/:date', async (req, res) => {
+    const { date } = req.params;
+    try {
+        const weatherDataResponse = await getWeatherDataUseCase.executeByDate(new Date(date));
+        res.json(weatherDataResponse);
+    } catch (error) {
+        console.error('Error in fetching weather data by date:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.get('/weather/lk/byOrder/:order/:weatherType', async (req, res) => {
+
+    const { order, weatherType } = req.params;
+
+    try {
+        const weatherDataResponse = await getWeatherDataUseCase.executeByOrder(order, weatherType);
+        res.json(weatherDataResponse);
+    } catch (error) {
+        console.error('Error in fetching weather data by order:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.get('/weather/lk/byValue/:value/:stack/:weatherType/:isDistrictsOnly', async (req, res) => {
+    const { value, stack, weatherType, isDistrictsOnly } = req.params;
+    try {
+        const weatherDataResponse = await getWeatherDataUseCase.executeByValue(parseFloat(value), parseInt(stack), weatherType, isDistrictsOnly === 'true');
+        res.json(weatherDataResponse);
+    } catch (error) {
+        console.error('Error in fetching weather data by value:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.get('/weather/lk/byDistrict/:district', async (req, res) => {
+    const { district } = req.params;
+    try {
+        const weatherDataResponse = await getWeatherDataUseCase.executeByDistrict(district);
+        res.json(weatherDataResponse);
+    } catch (error) {
+        console.error('Error in fetching weather data by district:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = router;
+
