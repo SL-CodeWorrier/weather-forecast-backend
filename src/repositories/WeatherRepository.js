7d93cfbe812bb038a13c0e34c683cb1abcfb312f
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-04-04 17:59:11 +0530

    modifed repository and useCase

--- src/repositories/WeatherRepository.js
+++ src/repositories/WeatherRepository.js
@@ -3,7 +3,7 @@ const ServiceResponse = require('../entities/ServiceResponse');
 const cron = require('node-cron');
 
 // Connection URI
-const uri = 'mongodb+srv://lakshitha3015:laksitha123@weatherdataclustor.ffxzqvm.mongodb.net/?retryWrites=true&w=majority&appName=weatherDataClustor';
+const uri = '';
 const client = new MongoClient(uri);
 
 async function connectToDatabase() {
@@ -12,27 +12,6 @@ async function connectToDatabase() {
         console.log('Connected to the database');
     } catch (error) {
         console.error('Error connecting to the database:', error);
-    }
-}
-
-// Function to connect to MongoDB and insert weather data
-async function insertWeatherData(weatherData) {
-    try {
-        const database = client.db('dbWeather');
-        const collection = database.collection('weatherData');
-
-        console.log('Random Weather Data:', weatherData);
-
-        const result = await collection.insertOne(weatherData);
-        console.log(result.insertedCount !== 'dataundefined' ? 'Weather data inserted successfully' : 'Failed to insert weather data');
-
-        const serviceResponse = new ServiceResponse(result.insertedCount > 0 ? 'success' : 'Fail', new Date(), weatherData, result.insertedCount > 0 ?  "Weather data saved successfully!" : "Cannot insert this record!");
-        return serviceResponse;
-        
-    } catch (error) {
-        console.error('Error inserting weather data:', error);
-        const serviceResponse = new ServiceResponse('fail', new Date(), error, "Failed to save weather data!");
-        return serviceResponse;
     }
 }
 
@@ -85,59 +64,6 @@ async function getWeatherData() {
     }
 }
 
-async function getWeatherDataByDate(date) {
-    try {
-        const database = client.db('dbWeather');
-        const collection = database.collection('weatherData');
-
-        const data = await collection.find({ lastUpdatedDateTime: date }).toArray();
-
-        const serviceResponse = new ServiceResponse('success', new Date(), data, "Weather data retrieved successfully by date!");
-        return serviceResponse;
-    } catch (error) {
-        const serviceResponse = new ServiceResponse('fail', new Date(), error, "Failed to retrieve weather data by date!");
-        return serviceResponse;
-    }
-}
-
-async function getWeatherDataByOrder(order, weatherType) {
-    try {
-        const database = client.db('dbWeather');
-        const collection = database.collection('weatherData');
-
-        const sortOptions = {};
-        sortOptions[weatherType] = order === 'Ascending' ? 1 : -1;
-
-        const data = await collection.find({}).sort(sortOptions).toArray();
-
-        const serviceResponse = new ServiceResponse('success', new Date(), data, "Weather data retrieved successfully by order!");
-        return serviceResponse;
-    } catch (error) {
-        const serviceResponse = new ServiceResponse('fail', new Date(), error, "Failed to retrieve weather data by order!");
-        return serviceResponse;
-    }
-}
-
-async function getWeatherDataByValue(value, stack, weatherType, isDistrictsOnly) {
-    try {
-        const database = client.db('dbWeather');
-        const collection = database.collection('weatherData');
-
-        const query = {};
-        query[weatherType] = stack === 1 ? { $gt: value } : { $lt: value };
-
-        const projection = isDistrictsOnly ? { district: 1, _id: 0 } : {};
-
-        const data = await collection.find(query, projection).toArray();
-
-        const serviceResponse = new ServiceResponse('success', new Date(), data, "Data retrieved successfully by value!");
-        return serviceResponse;
-    } catch (error) {
-        const serviceResponse = new ServiceResponse('fail', new Date(), error, "Failed to retrieve data by value!");
-        return serviceResponse;
-    }
-}
-
 async function getWeatherDataByDistrict(district) {
 
     try {
@@ -170,11 +96,7 @@ async function getWeatherDataByDistrict(district) {
 
 module.exports = {
     connectToDatabase,
-    insertWeatherData,
     updateWeatherDataByDistrict,
     getWeatherDataByDistrict,
-    getWeatherData,
-    getWeatherDataByDate,
-    getWeatherDataByOrder,
-    getWeatherDataByValue,
+    getWeatherData
 };
--- src/useCases/GetWeatherDataUseCase.js
+++ src/useCases/GetWeatherDataUseCase.js
@@ -7,16 +7,6 @@ class GetWeatherDataUseCase {
         this.weatherRepository = WeatherRepository
     }
 
-    async executeInsertWeatherData(weatherData){
-        try {
-            const weatherDataResponse = await this.weatherRepository.insertWeatherData(weatherData);
-            return weatherDataResponse;
-        } catch (error) {
-            console.error('Error in GetWeatherDataUseCase:', error);
-            return new ServiceResponse('fail', new Date(), error, "Failed to execute GetWeatherDataUseCase!");
-        }
-    }
-
     async executeUpdateWeatherDataByDistrict(district, weatherData) {
         try {
             const weatherDataResponse = await this.weatherRepository.updateWeatherDataByDistrict(district, weatherData);
@@ -35,39 +25,6 @@ class GetWeatherDataUseCase {
             // Handle error
             console.error('Error in GetWeatherDataUseCase:', error);
             return new ServiceResponse('fail', new Date(), error, "Failed to execute GetWeatherDataUseCase!");
-        }
-    }
-
-    async executeByDate(date) {
-        try {
-            const weatherDataResponse = await this.weatherRepository.getWeatherDataByDate(date);
-            return weatherDataResponse;
-        } catch (error) {
-            // Handle error
-            console.error('Error in GetWeatherDataByDateUseCase:', error);
-            return new ServiceResponse('fail', new Date(), error, "Failed to execute GetWeatherDataByDateUseCase!");
-        }
-    }
-
-    async executeByOrder(order, weatherType) {
-        try {
-            const weatherDataResponse = await this.weatherRepository.getWeatherDataByOrder(order, weatherType);
-            return weatherDataResponse;
-        } catch (error) {
-            // Handle error
-            console.error('Error in GetWeatherDataByOrderUseCase:', error);
-            return new ServiceResponse('fail', new Date(), error, "Failed to execute GetWeatherDataByOrderUseCase!");
-        }
-    }
-
-    async executeByValue(value, stack, weatherType, isDistrictsOnly) {
-        try {
-            const weatherDataResponse = await this.weatherRepository.getWeatherDataByValue(value, stack, weatherType, isDistrictsOnly);
-            return weatherDataResponse;
-        } catch (error) {
-            // Handle error
-            console.error('Error in GetWeatherDataByValueUseCase:', error);
-            return new ServiceResponse('fail', new Date(), error, "Failed to execute GetWeatherDataByValueUseCase!");
         }
     }
 
