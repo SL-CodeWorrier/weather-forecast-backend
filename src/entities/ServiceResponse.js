fe90af8ad8d9dc8f8f40f4b3bdcfe3f24cbb33c7
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-04-04 17:22:44 +0530

    added constructor for service response.js file

---
+++ src/entities/ServiceResponse.js
@@ -0,0 +1,10 @@
+class ServiceResponse {
+    constructor(status, requestedDatetime, data, message) {
+        this.status = status;
+        this.requestedDatetime = requestedDatetime;
+        this.data = data;
+        this.message = message
+    }
+
+
+}
