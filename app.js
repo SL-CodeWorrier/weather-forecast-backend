72993a53c591c60ad871b813a4c7e470de8b441f
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-04-04 17:38:45 +0530

    app.js updated

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const weatherController = require('./src/controllers/WeatherController');
+
+app.use(cors());
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/v1', weatherController);
+
+// Start the server
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
